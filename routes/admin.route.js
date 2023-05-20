7a0755fb78a5b93889739f08eddc81c76335aae3
GustavoResendizCast <124696432+GustavoResendizCast@users.noreply.github.com>
2023-05-20 22:18:03 +0000

    Creando vistas

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,26 +1,16 @@
 // Importando el enrutador de express
 import { Router } from 'express';
+// Importando el gestor de rutas
+import path from 'path';
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// Exportando el enrutador admin
-export default router;
-
 // GET /add-product
 router.get('/add-product', (req, res, next) => {
-  // Si la petición es post pasamos el siguiente
-  // Middleware
-  if(req.method === "POST") return next();
-
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  res.sendFile(path.resolve('views','add-product.html'));
 });
 
 // POST /add-product
@@ -30,3 +20,6 @@ router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
+
+// Exportando el enrutador admin
+export default router;
